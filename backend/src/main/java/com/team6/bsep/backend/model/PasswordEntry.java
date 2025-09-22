b453207c78363e13395e5f2ab35689b06df4698a
Maša Mastilović <masamastilovic@gmail.com>
2025-09-22 12:27:57 +0200

    feat: model for shared password manager

---
+++ backend/src/main/java/com/team6/bsep/backend/model/PasswordEntry.java
@@ -0,0 +1,34 @@
+package com.team6.bsep.backend.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.*;
+
+@Entity
+@Table(name = "password_entries")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PasswordEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String siteName;
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;  // EE korisnik (iz PKI sistema)
+
+    private LocalDateTime createdAt;
+
+
+    @OneToMany(mappedBy = "passwordEntry", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PasswordShare> shares;
+
+}
---
+++ backend/src/main/java/com/team6/bsep/backend/model/PasswordShare.java
@@ -0,0 +1,33 @@
+package com.team6.bsep.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "password_shares")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PasswordShare {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // referenca na lozinku
+    @ManyToOne
+    @JoinColumn(name = "entry_id", nullable = false)
+    private PasswordEntry passwordEntry;
+
+    // kome je podeljeno
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Lob
+    @Column(nullable = false)
+    private String encryptedPassword;
+
+
+}
